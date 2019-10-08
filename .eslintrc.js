40b9429c193f418aa54f7ac8867e2dbb4291a174
andbilous <andbilous@gmail.com>
2019-10-08 12:07:03 +0300

    adding hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  "extends": ["airbnb", "prettier"],
-  "plugins": ["prettier"],
-  "rules": {
-    "prettier/prettier": ["error"]
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off'
+  },
+  'globals': {
+    "fetch": false
   }
 }
--- components/CardTypeDefiner.component.js
+++ components/CardTypeDefiner.component.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { TextInput, View , StyleSheet} from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import CardTypeOutput from './CardTypeOutput.component';
 
 type Props = {
@@ -14,24 +14,27 @@ type State = {
 
 class CardTypeDefiner extends React.Component<Props, State> {
   static defaultProps = {
-    cardNumber:111111111111111
+    cardNumber: 111111111111111
   }
 
   render() {
-    const {cardNumber}= this.props;
-    const lastFourDigits = parseInt(cardNumber.toString().substr(cardNumber.toString().length-4,cardNumber.toString().length));
+    const { cardNumber } = this.props;
+    const lastFourDigits = parseInt(cardNumber.toString().substr(cardNumber.toString().length - 4, cardNumber.toString().length));
     const cardType = lastFourDigits < 2000 ? 'MasterCard' : 'Visa';
     return (
-        <View style={styles.container}>
-          <CardTypeOutput cardType={cardType}/>
-        </View>
+      <View style={styles.container}>
+        <CardTypeOutput cardType={cardType} />
+      </View>
     );
   }
 }
+
+
+
 const styles = StyleSheet.create({
-    container:{
-      backgroundColor:'#e39612'
-    }
+  container: {
+    backgroundColor: '#e39612'
+  }
 });
 
 export default CardTypeDefiner;
